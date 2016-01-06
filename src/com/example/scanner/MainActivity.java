66bf607f9f6511393d0e7d69d4261b2fcf9cc759
amngupta <amangupta96@outlook.com>
2016-01-06 16:40:41 +0530

    Attempts to Encrypt User data

--- src/com/example/scanner/MainActivity.java
+++ src/com/example/scanner/MainActivity.java
@@ -2,21 +2,33 @@ package com.example.scanner;
 
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.Key;
+import java.security.MessageDigest;
 
+import javax.crypto.*;
+import javax.crypto.spec.DESedeKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SpinnerAdapter;
 import android.widget.Toast;
@@ -33,7 +45,9 @@ public class MainActivity extends Activity {
 	static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
 	private static final String PREF_USERNAME = "username:";
 	private static final String PREF_PASSWORD = "password:";
+	private static final String PREF_KEY = "key:";
 
+	private static String stringKey = null;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -41,6 +55,24 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
+	//	void createButtons()
+	//	{
+	//		Resources res = getResources();
+	//		String[] accounts = res.getStringArray(R.array.accounts);
+	//		for (String account: accounts)
+	//		{
+	//			Log.w("accounts", account);
+	//			Button btn= new Button(this);  
+	//			btn.setText(account);  
+	//			btn.setOnClickListener(new View.OnClickListener()   
+	//			{
+	//			    public void onClick(View view) 
+	//			     {
+	//			           scanQR(view);
+	//			     }
+	//			});
+	//		}		
+	//	}
 
 	//product qr code mode
 	public void scanQR(View v) {
@@ -54,12 +86,17 @@ public class MainActivity extends Activity {
 			showDialog(MainActivity.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
 		}
 	}
-
+	/*
+	 * Open the NewAccount acitivity
+	 */
 	public void addAccount(View v)
 	{
-
 		setContentView(R.layout.activity_new_account);
 	}
+
+	/*
+	 * Store the Data in Mobile Memory
+	 */
 	public void addData(View v)
 	{
 		EditText username = (EditText) findViewById(R.id.username);
@@ -67,22 +104,96 @@ public class MainActivity extends Activity {
 		String user = username.getText().toString();
 		String pass = password.getText().toString();
 		@SuppressWarnings("unchecked")
-		String account = ((AdapterView<SpinnerAdapter>) findViewById(R.id.spinner)).getSelectedItem().toString();
+		String account = ((AdapterView<SpinnerAdapter>) findViewById(R.id.spinner)).getSelectedItem().toString().toLowerCase();
 		if (user.length()>0 && pass.length()>0)
 		{
-			getSharedPreferences(account,MODE_PRIVATE)
-			.edit()
-			.putString(PREF_USERNAME, user)
-			.putString(PREF_PASSWORD, pass)
-			.commit();
-			Toast.makeText(getApplicationContext(),"Saved Successfully",Toast.LENGTH_LONG).show();
-			setContentView(R.layout.activity_main);
+			String encryptedUser = encrypt(user);
+			String encryptedPass = encrypt(pass);
+			if(stringKey != null){
+				getSharedPreferences(account,MODE_PRIVATE)
+				.edit()
+				.putString(PREF_USERNAME, user)
+				.putString(PREF_PASSWORD, encryptedPass)
+				.putString(PREF_KEY, stringKey)
+				.commit();
+				Toast.makeText(getApplicationContext(),"Saved Successfully",Toast.LENGTH_LONG).show();
+				setContentView(R.layout.activity_main);
+				stringKey=null;
+			}
+		}
+		else if (user.length() >0 && pass.length() == 0 )
+		{
+			Toast.makeText(getApplicationContext(),"Please Enter Password for " + account,Toast.LENGTH_LONG).show();
+		}
+		else if (user.length() == 0 && pass.length() > 0 )
+		{
+			Toast.makeText(getApplicationContext(),"Please Enter Username for " + account,Toast.LENGTH_LONG).show();
 		}
 		else
 		{
-			Toast.makeText(getApplicationContext(),"Please Enter All Details",Toast.LENGTH_LONG).show();
+			Toast.makeText(getApplicationContext(),"Please Enter Login Details for " + account,Toast.LENGTH_LONG).show();
 		}
 	}
+	/*
+	 * Method to encrypt a String using AES encryption
+	 */
+	private String encrypt(String plainData)
+	{
+		try {
+			KeyGenerator keyGen = KeyGenerator.getInstance("AES"); 
+			keyGen.init(128); 
+			SecretKey secretKey = keyGen.generateKey(); 
+			Cipher aesCipher = Cipher.getInstance("AES");
+			aesCipher.init(Cipher.ENCRYPT_MODE,secretKey); 
+			byte[] byteCipherText = aesCipher.doFinal(plainData.getBytes()); 
+			String encrypted =  Base64.encodeToString(byteCipherText, Base64.DEFAULT);
+			stringKey = Base64.encodeToString(secretKey.getEncoded(), Base64.DEFAULT);
+			return encrypted;
+		}
+		catch(Exception e) { } 
+		return null;
+	}
+	private String encrypt(String plainData, String key)
+	{
+		try{
+			final MessageDigest md = MessageDigest.getInstance("MD5");
+			final byte[] digest = md.digest(key.getBytes());
+
+			SecretKey secretKey = new SecretKeySpec(digest, "AES");
+			Cipher aesCipher = Cipher.getInstance("AES");
+			aesCipher.init(Cipher.ENCRYPT_MODE,secretKey); 
+			byte[] byteCipherText = aesCipher.doFinal(plainData.getBytes()); 
+			String encrypted =  Base64.encodeToString(byteCipherText, Base64.DEFAULT);
+			Toast.makeText(this,"Message from Server: \n"+ encrypted, 0).show();             
+
+			return encrypted;	
+		}catch(Exception e)
+		{
+			Toast.makeText(this,"Exception Here", 0).show();             
+
+		}
+		return null;
+	}
+	private  String decrypt(String encryptedText, String key)
+	{
+		try{
+			SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+			// Instantiate the cipher
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+			byte[] encryptedTextBytes = Base64.decode(encryptedText, Base64.DEFAULT);
+			byte[] decryptedTextBytes = cipher.doFinal(encryptedTextBytes);
+			String decrypted = new String(decryptedTextBytes);
+			Toast.makeText(this,"Message from Server: \n"+ decrypted, 0).show();             
+
+			return decrypted;
+		}
+		catch(Exception e)
+		{}
+		return null;
+	}
+
 	//alert dialog for downloadDialog
 	private static AlertDialog showDialog(final Activity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo) {
 		AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
@@ -106,7 +217,6 @@ public class MainActivity extends Activity {
 		return downloadDialog.show();
 	}
 
-
 	//on ActivityResult method
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		if (requestCode == 0) {
@@ -114,39 +224,44 @@ public class MainActivity extends Activity {
 				//get the extras that are returned from the intent
 				final String recievedString = intent.getStringExtra("SCAN_RESULT");
 				String[] parts = recievedString.split("::");
-				final String account=parts[0].toLowerCase();
+				final String account=parts[0];
 				final String code=parts[1];
-				
-				SharedPreferences settings = getSharedPreferences(account, MODE_PRIVATE);
-				final String user;
-				final String pass;
-				user = settings.getString(PREF_USERNAME, null);
-				pass = settings.getString(PREF_PASSWORD, null);
-//				Toast.makeText(this,"Message from Server: \n"+ recievedString, 0).show();             
 
+				SharedPreferences settings = getSharedPreferences(account, MODE_PRIVATE);
+				final String user  = settings.getString(PREF_USERNAME, null);
+				String pass = settings.getString(PREF_PASSWORD, null);
+				final String key = settings.getString(PREF_KEY, null);
+				final String decryptPass = decrypt(pass, key);
+				Toast.makeText(this,"Message from Server: \n"+ decryptPass, 0).show();             
 				new Thread(){
 					public void run(){
-						sendData(account, code, user, pass);				    
-						}
+						sendData(account,code, user, decryptPass);				    
+					}
 				}.start();
 			}
 		}
 	}
+
+	/*
+	 * Method to Post to Server
+	 */
+	@SuppressLint("ShowToast")
 	void sendData(String account, String code, String user, String pass)
 	{
+
 		HttpURLConnection connection;
 		OutputStreamWriter request = null;
 
 		URL url = null;   
 		String response = null; 
-		String data = "account:" + account + ",code:" + code + ",username:" + user + ",password:" + pass;
-		accountDetails temp = new accountDetails();
-		temp.account = account;
-		temp.code = code;
-		temp.password = pass;
-		temp.username = user;
+		String password = null;
+
 		try
 		{
+			password = encrypt(pass,code);
+
+			String data = "account!!!" + account + ",code!!!" + code + ",username!!!" + user + ",password!!!" + password;
+
 			url = new URL("https://phone-unlock.herokuapp.com/login");
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setDoOutput(true);
@@ -174,7 +289,7 @@ public class MainActivity extends Activity {
 		}
 		catch(Exception e)
 		{
-			
+
 		}
 	}
 }
